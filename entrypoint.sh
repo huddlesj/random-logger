7e267246aa5fff17a0765dcadb0ad03650c8cd5d
Jason Huddleston <huddlesj@gmail.com>
2019-03-05 10:04:07 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ do
    waitTime=$(shuf -i 1-5 -n 1)
    sleep $waitTime &
    wait $!
-   instruction=$(shuf -i 0-4 -n 1)
+   #instruction=$(shuf -i 0-4 -n 1)
+   instruction=4
    d=`date -Iseconds`
    case "$instruction" in
       "1") echo "$d ERROR something happened in this execution."
